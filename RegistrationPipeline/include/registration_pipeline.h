fc37ad9abab4621b985990cb4428a21b0ac68625
Haowen Xu <haowen.xu@pku.edu.cn>
2018-04-29 05:12:21 +0800

    added pipeline of trimming

--- RegistrationPipeline/include/registration_pipeline.h
+++ RegistrationPipeline/include/registration_pipeline.h
@@ -2,9 +2,13 @@
 #define REGISTRATION_PIPELINE_H
 #include "prereq.h"
 
+#include <vector>
+#include <string>
+
 namespace RegPipeline
 {
-	REGPIPELINE_PUBLIC void Test() {}
+  // Trim two points clouds
+  REGPIPELINE_PUBLIC void TrimPointsClouds(const std::vector<std::string> &filenames, std::string &in_format);
 }
 
 #endif
--- RegistrationPipeline/src/registration_pipeline.cpp
+++ RegistrationPipeline/src/registration_pipeline.cpp
@@ -1,1 +1,31 @@
 #include "registration_pipeline.h"
+
+#include <igl/readOBJ.h>
+#include <igl/writeOBJ.h>
+#include <DataIO/include/data_io.h>
+#include <OverlapTrimmer/include/trimmer.h>
+
+REGPIPELINE_PUBLIC void RegPipeline::TrimPointsClouds(const std::vector<std::string> &filenames,
+  std::string &in_format)
+{
+  if (filenames.size() < 4) return;
+
+  Eigen::MatrixXd A, B;
+  if (in_format == "OBJ" || in_format == "obj")
+  {
+    Eigen::MatrixXi F;
+    igl::readOBJ(filenames[0], A, F);
+    igl::readOBJ(filenames[1], B, F);
+  }
+  if (in_format == "PLY" || in_format == "ply")
+  {
+    Eigen::MatrixXd C;
+    DataIO::read_ply(filenames[0], A, C);
+    DataIO::read_ply(filenames[1], B, C);
+  }
+
+  Trimmer::Trim(A, B, 0.05);
+
+  igl::writeOBJ(filenames[2], A, Eigen::MatrixXi());
+  igl::writeOBJ(filenames[3], B, Eigen::MatrixXi());
+}
