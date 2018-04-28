0081cf3294ab30845c167972ec0de172c95e5b16
Haowen Xu <haowen.xu@pku.edu.cn>
2018-04-29 00:51:11 +0800

    added localizer part (without parsing json and exif)

--- GPSLocalizer/include/gps.h
+++ GPSLocalizer/include/gps.h
@@ -2,9 +2,15 @@
 #define GPS_H
 #include "prereq.h"
 
+#include <Eigen/dense>
+
 namespace GPS
 {
-	GPS_PUBLIC void Test(){}
+  GPS_PUBLIC void CalcualteCamerasTransformation(const Eigen::MatrixXd &X, const Eigen::MatrixXd &Y, double &s,
+    Eigen::Matrix3d &R, Eigen::RowVector3d &T);
+  GPS_PUBLIC Eigen::MatrixXd GetLocalizedPoints(const Eigen::MatrixXd &X, const double &s, const Eigen::Matrix3d &R,
+    const Eigen::RowVector3d &T);
+  GPS_PUBLIC void Localize();
 }
 
 #endif
--- GPSLocalizer/src/gps.cpp
+++ GPSLocalizer/src/gps.cpp
@@ -1,1 +1,23 @@
 #include "gps.h"
+
+#include <Eigen/dense>
+#include "./ScaleStretchICP/include/ssicp.h"
+
+GPS_PUBLIC void GPS::CalcualteCamerasTransformation(const Eigen::MatrixXd &X, const Eigen::MatrixXd& Y, double &s,
+  Eigen::Matrix3d &R, Eigen::RowVector3d &T)
+{
+  double a, b;
+  SSICP::Initialize(X, Y, s, a, b, R, T);
+  SSICP::FindTransformation(X, Y, a, b, s, R, T);
+}
+
+GPS_PUBLIC Eigen::MatrixXd GPS::GetLocalizedPoints(const Eigen::MatrixXd &X, const double &s, const Eigen::Matrix3d &R,
+  const Eigen::RowVector3d &T)
+{
+  return SSICP::GetTransformed(X, s, R, T);
+}
+
+GPS_PUBLIC void GPS::Localize()
+{
+
+}
