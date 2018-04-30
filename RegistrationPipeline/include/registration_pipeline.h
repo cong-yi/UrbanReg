8cbebb6334c5a8be3a99fa5624aa8607621f6ba4
Haowen Xu <haowen.xu@pku.edu.cn>
2018-04-30 22:34:28 +0800

    added localize pipeline

--- RegistrationPipeline/include/registration_pipeline.h
+++ RegistrationPipeline/include/registration_pipeline.h
@@ -7,7 +7,18 @@
 
 namespace RegPipeline
 {
+  // Relocate point cloud using GPS data
+  // filenames[0]: input filename of point cloud
+  // filenames[1]: local coordinates of cameras
+  // filenames[2]: global coordinates of cameras
+  // filenames[3]: output filename of transformed point cloud
+  REGPIPELINE_PUBLIC void LocalizePointCloud(const std::vector<std::string> &filenames, std::string &format);
+
   // Trim two points clouds
+  // filenames[0]: input filename of point cloud a
+  // filenames[1]: input filename of point cloud b
+  // filenames[2]: output filename of point cloud a
+  // filenames[3]: output filename of point cloud b
   REGPIPELINE_PUBLIC void TrimPointsClouds(const std::vector<std::string> &filenames, std::string &in_format);
 }
 
--- RegistrationPipeline/src/registration_pipeline.cpp
+++ RegistrationPipeline/src/registration_pipeline.cpp
@@ -3,7 +3,41 @@
 #include <igl/readOBJ.h>
 #include <igl/writeOBJ.h>
 #include <DataIO/include/data_io.h>
+#include <GPSLocalizer/include/gps.h>
 #include <OverlapTrimmer/include/trimmer.h>
+
+REGPIPELINE_PUBLIC void RegPipeline::LocalizePointCloud(const std::vector<std::string> &filenames, std::string &format)
+{
+  if (filenames.size() < 4) return;
+  
+  Eigen::MatrixXd A;
+  if (format == "OBJ" || format == "obj")
+  {
+    Eigen::MatrixXi F;
+    igl::readOBJ(filenames[0], A, F);
+  }
+  if (format == "PLY" || format == "ply")
+  {
+    Eigen::MatrixXd C;
+    DataIO::read_ply(filenames[0], A, C);
+  }
+
+  Eigen::MatrixXd L, G;
+  Eigen::MatrixXi F;
+  igl::readOBJ(filenames[1], L, F);
+  igl::readOBJ(filenames[2], G, F);
+
+  double s;
+  Eigen::Matrix3d R;
+  Eigen::RowVector3d T;
+  GPS::CalcualteCamerasTransformation(L, G, s, R, T);
+  Eigen::MatrixXd B = GPS::GetLocalizedPoints(A, s, R, T);
+
+  if (format == "OBJ" || format == "obj")
+    igl::writeOBJ(filenames[3], B, Eigen::MatrixXi());
+  if (format == "PLY" || format == "ply")
+    DataIO::write_ply(filenames[3], B, Eigen::MatrixXd());
+}
 
 REGPIPELINE_PUBLIC void RegPipeline::TrimPointsClouds(const std::vector<std::string> &filenames,
   std::string &in_format)
